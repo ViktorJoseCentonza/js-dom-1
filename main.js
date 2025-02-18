b1a01740340c8c084118b98647730adf22c9074a
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-18 21:35:56 +0100

    fix img path (first method)

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ document.getElementById("mybutton").addEventListener("click", switch_img)
 function switch_img() {
 
     if (active == false) {
-        document.getElementById("myimg").src = "/assets/img/yellow_lamp_aligned.png"
+        document.getElementById("myimg").src = "./assets/img/yellow_lamp_aligned.png"
         document.getElementById("mybutton").value = "Spegni"
         active = true
     } else {
-        document.getElementById("myimg").src = "/assets/img/white_lamp.png"
+        document.getElementById("myimg").src = "./assets/img/white_lamp.png"
         document.getElementById("mybutton").value = "Accendi"
         active = false
     }
