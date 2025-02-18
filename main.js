e49ecc4bad5324204f018c9b6a40d5f1edb9aade
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-18 15:47:08 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+let active = false;
+document.getElementById("mybutton").addEventListener("click", switch_img)
+
+function switch_img() {
+
+    if (active == false) {
+        document.getElementById("myimg").src = "/assets/img/yellow_lamp.png"
+        document.getElementById("mybutton").value = "Spegni"
+        active = true
+    } else {
+        document.getElementById("myimg").src = "/assets/img/white_lamp.png"
+        document.getElementById("mybutton").value = "Accendi"
+        active = false
+    }
+
+
+
+    return 0
+} 
