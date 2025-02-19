f3de9bbc6ffaeefbb6277b1d28d1ed4b443de96d
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-19 09:54:55 +0100

    debug add third method comment out d-none img

--- main.js
+++ main.js
@@ -18,12 +18,29 @@ function switch_img() {
 */
 
 ///second method
-
+/*
 document.getElementById("mybutton").addEventListener("click", switch_img)
 
 function switch_img() {
 
     if (document.getElementById("mybutton").value.includes("Accendi")) {
+        document.getElementById("myimg").src = "./assets/img/yellow_lamp.png"
+        document.getElementById("mybutton").value = "Spegni"
+    } else {
+        document.getElementById("myimg").src = "./assets/img/white_lamp.png"
+        document.getElementById("mybutton").value = "Accendi"
+    }
+} 
+
+*/
+///third method (trying to fix not loading alt image on server-side)
+
+
+document.getElementById("mybutton").addEventListener("click", switch_img)
+
+function switch_img() {
+
+    if (document.getElementById("myimg").src.includes("white_lamp.png")) {
         document.getElementById("myimg").src = "./assets/img/yellow_lamp.png"
         document.getElementById("mybutton").value = "Spegni"
     } else {
